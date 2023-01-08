40c49bcbd90a725e96b509c90987482e8d709305
Michael Clermont <michael.clermont@mths.ca>
2023-01-08 17:25:51 +0000

    edited binary search program

--- BinarySearch.java
+++ BinarySearch.java
@@ -54,15 +54,14 @@ final class BinarySearch {
                             final int lowIndex, final int highIndex) {
         if (lowIndex > highIndex) {
             return -1;
+        }
+        final int mid = (lowIndex + highIndex) / 2;
+        if (userArray[mid] == userNumber) {
+            return mid;
+        } else if (userNumber < userArray[mid]) {
+            return binarySearch(userArray, userNumber, lowIndex, highIndex - 1);
         } else {
-            final int mid = (lowIndex + highIndex) / 2;
-            if (userNumber == userArray[mid]) {
-                return mid;
-            } else if (userNumber > userArray[mid]) {
-                return binarySearch(userArray, userNumber, mid + 1, highIndex);
-            } else {
-                return binarySearch(userArray, userNumber, lowIndex, mid - 1);
-            }
+            return binarySearch(userArray, userNumber, mid + 1, highIndex);
         }
     }
 
@@ -73,56 +72,47 @@ final class BinarySearch {
     */
     public static void main(final String[] args) {
         System.out.println("Binary Search Program.");
-        try {
-            // Initializing the random class
-            final Random randNumber = new Random();
+        // Initializing the random class
+        final Random randNumber = new Random();
+        // Initializing array of numbers
+        final int[] randomNumberArray = new int[ARRAY_SIZE];
+        // Adding numbers to the array
+        for (int counter = 0; counter < randomNumberArray.length;
+                       counter++) {
+            randomNumberArray[counter] = randNumber.nextInt(MAX) + 1;
+        }
 
-            // Initializing array of numbers
-            final int[] randomNumberArray = new int[ARRAY_SIZE];
+        // Sorting the array
+        final int[] numberArray = randomNumberArray;
+        Arrays.sort(numberArray);
 
-            // Adding numbers to the array
-            for (int counter = 0; counter < randomNumberArray.length;
-                            counter++) {
-                randomNumberArray[counter] = randNumber.nextInt(MAX) + 1;
-            }
+        System.out.print("\nSorted list of numbers:\n");
+        for (int element: numberArray) {
+            final String padded = String.format("%03d", element);
+            System.out.print(padded + ", ");
+        }
+        System.out.print("\n\n");
 
-            // Sorting the array
-            final int[] numberArray = randomNumberArray;
-            Arrays.sort(numberArray);
+        // Getting user input as to what number they wish to search for
+        final Scanner userInput = new Scanner(System.in);
+        System.out.print("What number are you searching for in the array");
+        System.out.print(" (integer between 0 and 999): ");
+        final int searchNumber = userInput.nextInt();
+        userInput.close();
+        System.out.println();
 
-            System.out.print("\nSorted list of numbers:\n");
-            for (int element: numberArray) {
-                final String padded = String.format("%03d", element);
-                System.out.print(padded + ", ");
-            }
-            System.out.print("\n\n");
+        // Ensuring the user inputs an appropriate integer
+        if (searchNumber > MAX || searchNumber < MIN) {
+            System.out.println("ERROR");
+        } else {
+            // Using binary search to find the user's chosen number in the
+            // array
+            final int searchResult = binarySearch(numberArray, searchNumber,
+                                        0, numberArray.length - 1);
 
-            // Getting user input as to what number they wish to search for
-            final Scanner userInput = new Scanner(System.in);
-            System.out.print("What number are you searching for in the array");
-            System.out.print(" (integer between 0 and 999): ");
-            final int searchNumber = userInput.nextInt();
-            userInput.close();
+            // Outputing the results of the search
             System.out.println();
-
-            // Ensuring the user inputs an appropriate integer
-            if (searchNumber > MAX || searchNumber < MIN) {
-                throw new Exception();
-            } else {
-                // Using binary search to find the user's chosen number in the
-                // array
-                final int searchResult = binarySearch(numberArray, searchNumber,
-                                                0, numberArray.length - 1);
-
-                // Outputing the results of the search
-                System.out.println();
-                System.out.println("Your number is in index: " + searchResult);
-            }
-
-            // Catches and tells the user that an error occured
-        } catch (Exception e) {
-            System.out.println();
-            System.out.println("ERROR: Invalid Input");
+            System.out.println("Your number is in index: " + searchResult);
         }
     }
 }
